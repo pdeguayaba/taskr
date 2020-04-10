506836fbe5bb094744d0970e77a8097c8212f90d
Edwin J. Estrella Ayala <edwinjestrella@yahoo.com>
2020-04-10 19:53:06 -0400

    Added task, table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_10_201811) do
+ActiveRecord::Schema.define(version: 2020_04_10_233906) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
